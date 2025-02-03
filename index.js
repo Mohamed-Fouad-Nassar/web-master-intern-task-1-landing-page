a33b646faa43ed03410d627e6f4da54b4778945f
Mohammed Nassar <mohammednassar740@gmail.com>
2025-02-03 19:54:47 +0200

    add final touches to the design

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const menuToggleBtn = document.querySelector(".mobile-menu-toggle");
+const menuOpenBtn = document.querySelector(".mobile-menu-open");
+const menuCloseBtn = document.querySelector(".mobile-menu-close");
 const menu = document.querySelector("header nav");
+const overlay = document.querySelector("header nav .overlay");
 
-menuToggleBtn.addEventListener("click", () => {
-  menu.classList.toggle("open");
+menuOpenBtn.addEventListener("click", () => {
+  menu.classList.add("open");
+});
+menuCloseBtn.addEventListener("click", () => {
+  menu.classList.remove("open");
+});
+
+overlay.addEventListener("click", () => {
+  menu.classList.remove("open");
 });
